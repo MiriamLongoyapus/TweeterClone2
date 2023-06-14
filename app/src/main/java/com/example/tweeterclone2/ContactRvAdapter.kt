754a8648c43c2ae343398eb472dd30bc67d7a666
Miriam <longoyapusmiriam@gmail.com>
2023-06-14 17:34:56 +0300

    change

--- app/src/main/java/com/example/tweeterclone2/ContactRvAdapter.kt
+++ app/src/main/java/com/example/tweeterclone2/ContactRvAdapter.kt
@@ -1,28 +1,66 @@
 package com.example.tweeterclone2
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.tweeterclone2.databinding.ContactListItemBinding
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.RequestCreator
 
-class ContactAdapter(var ContactList:List<ContactData>) : RecyclerView.Adapter<ContactViewHolder>(){
+class ContactAdapter(var ContactList:List<ContactData>) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
         val binding =
-            ContactListItemBinding .inflate(LayoutInflater.from(parent.context),parent ,false)
+            ContactListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ContactViewHolder(binding)
     }
+
     override fun getItemCount(): Int {
         return ContactList.size
     }
+//    override fun onResume() {
+//        super.onResume()
+//        displayContacts()
+//        binding.btnFloating.setOnClickListener {
+//            val intent = Intent(this,AddContacts::class.java)
+//            startActivity(intent)
+//        }
+//    }
+
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
 
-        var currentContact =ContactList.get(position)
-        var  binding=holder.binding
-        binding.tvDisplayName.text=currentContact.toString()
-        binding.tvPhoneNumber.text=currentContact.toString()
-        binding.tvEmailAddress.text =currentContact.toString()
+        var currentContact = ContactList.get(position)
+        var binding = holder.binding
+        binding.ivAvator.tag=currentContact.avatar
+        binding.tvDisplayName.text = currentContact.displayName
+        binding.tvPhoneNumber.text = currentContact.phoneNumber
+        binding.tvEmailAddress.text = currentContact.emailAddress
+
+        binding.btnAdd.setOnClickListener {
+            val intent = Intent(holder.itemView.context, MainActivity2::class.java)
+            holder.itemView.context.startActivity(intent)
+        }
+        Picasso
+            .get()
+            .load(currentContact.avatar)
+            .resize(280, 280)
+            .centerCrop()
+            .into(binding.ivAvator)
+//        binding.btnAdd.setOnClickListener {
+//            val intent = Intent(this,MainActivity2::class.java)
+//          b(intent)
+//        }
     }
+
+
+
+
+
+
+
+    class ContactViewHolder(var binding: ContactListItemBinding) : ViewHolder(binding.root)
 }
-class ContactViewHolder( var binding:ContactListItemBinding):ViewHolder(binding.root)
+
--- app/src/main/java/com/example/tweeterclone2/MainActivity.kt
+++ app/src/main/java/com/example/tweeterclone2/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tweeterclone2.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -15,17 +16,23 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         displayContact()
+
+//        binding...setOnClickListener {
+//            val intent = Intent(this,Contacts::class.java)
+//            startActivity(intent)
+//        }
     }
     fun displayContact(){
-        val contact1=ContactData("Cynthia","07886060933","cynthia@gmail.com")
-        val contact2=ContactData("mary","07895543392","mary@gmail.com")
-        val contact3=ContactData("Jane","0782364352","jane@gmail.com")
-        val contact4=ContactData("Jimbo","0756364355","jimboe@gmail.com")
-        val contact5=ContactData("Beryl","0782365368","beryl@gmail.com")
+        val contact1=ContactData("https://media.istockphoto.com/id/1403709099/photo/african-teenage.webp?b=1&s=170667a&w=0&k=20&c=b4ux5ZbPJFsSjo0b2-4PSXCuV5SdwuA6AQtI-mCUKoo= ","Cynthia","07886060933","cynthia@gmail.com")
+        val contact2= ContactData("https://media.istockphoto.com/id/644806678/photo/little-girl-posing-on-gray-background.webp?b=1&s=170667a&w=0&k=20&c=yI4pUFLALj6BlLSi0TyNrkbwIrrCzLqzwYYOrV8QeqE=","Mary","07895543392","mary@gmail.com")
+        val contact3=ContactData("https://images.unsplash.com/photo-1593878024377-b38927fc7689?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmxhY2slMjBnaXJsfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60","jane","0784556789","jane@gmail.com")
+        val contact4=ContactData("https://images.unsplash.com/photo-1522390108011-5f8667fd551d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmxhY2slMjBnaXJsfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60","jimbo","07567784399","jimbo@gmail.com")
+        val contact5=ContactData("https://media.istockphoto.com/id/1382459668/photo/pretty-young-afro-woman-among-palm-trees.webp?b=1&s=170667a&w=0&k=20&c=V_278p-swKuB4vD95jh5etDyHyzupc53WlDBnLbMGUY=","Berly","0792035101","berly@gmail.com")
         val contactList= listOf(contact1,contact2,contact3,contact4,contact5)
         val contactAdapter=ContactAdapter(contactList)
         binding.rvTweets.layoutManager=LinearLayoutManager(this)
         binding.rvTweets.adapter=contactAdapter
+
     }
 }
 
